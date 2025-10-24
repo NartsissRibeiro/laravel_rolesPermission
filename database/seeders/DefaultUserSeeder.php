218670ed0b11b3574eed167a1dc37c0ec5a09af8
neto <only.narciso042@gmail.com>
2025-10-24 20:50:30 -0300

    adicionando seeders

---
+++ database/seeders/DefaultUserSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class DefaultUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $superAdmin = User::create([
+            'name' => 'Super Admin',
+            'email' => 'teste@gmail.com',
+            'password' => Hash::make('teste123')
+        ]);
+        $superAdmin->assignRole('Super Admin');
+
+        $admin = User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin123')
+        ]);
+        $admin->assignRole('Admin');
+
+        $productManager = User::create([
+            'name' => 'Product Manager User',
+            'email' => 'product@gmail.com',
+            'password' => Hash::make('product123')
+        ]);
+        $productManager->assignRole('Product Manager');
+
+        $user = User::create([
+            'name' => 'Regular User',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('user123')
+        ]);
+        $user->assignRole('User');
+    }
+}
