1ed868c52a7c85baa1e5a7a24d02ea3a18c09715
neto <only.narciso042@gmail.com>
2025-11-28 20:26:54 -0300

    fazendo alteração

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,28 +13,30 @@
                             {{ session('status') }}
                         </div>
                     @endif
+
                     {{ __('Você está logado!') }}
                     <p>Menu</p>
                     @canany(['create-role', 'edit-role', 'delete-role'])
-                        <a class="btn btn-primary" href="{{route('roles.index')}}">
+                        <a class="btn btn-primary" href="{{ route('roles.index') }}">
                             <i class="bi bi-person-fill-gear"></i> Gestão de Papéis</a>
                     @endcanany
                     @canany(['create-user', 'edit-user', 'delete-user'])
-                        <a class="btn btn-success" href="{{route('users.index')}}">
-                            <i class="bi bi-people"></i> Gestão de Usuários</a>
+                        <a class="btn btn-success" href="{{ route('users.index') }}">
+                        <i class="bi bi-people"></i> Gestão de Usuários</a>
                     @endcanany
+
                     @canany(['create-product', 'edit-product', 'delete-product'])
-                        <a class="btn btn-warning" href="{{route('products.index') }}">
+                        <a class="btn btn-warning" href="{{ route('products.index') }}">
                             <i class="bi bi-bag"></i> Gestão de Produtos</a>
                     @endcanany
                     @can(['view-product'])
-                        <a class="btn btn-info" href="{{route('products.index')}}">
+                        <a class="btn btn-info" href="{{ route('products.index') }}">
                             <i class="bi bi-bag"></i> Visualizar Produtos</a>
-                        @endcan
-                        <p>&nbsp;</p>
-                    </div>
+                    @endcan
+                    <p>&nbsp;</p>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::resources([
     'users' => UserController::class,
     'products' => ProductController::class,
  ]);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
